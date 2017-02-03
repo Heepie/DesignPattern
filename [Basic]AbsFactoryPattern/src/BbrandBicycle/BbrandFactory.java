816816bed89ee2bc7d8ce2a691683a2cb02f75b5
heepie <gheepie@gmail.com>
2017-02-03 18:38:02 +0900

    to study AbsFactoryPattern

--- [Basic]AbsFactoryPattern/src/BbrandBicycle/BbrandFactory.java
+++ [Basic]AbsFactoryPattern/src/BbrandBicycle/BbrandFactory.java
@@ -1,5 +1,20 @@
 package BbrandBicycle;
 
-public class BbrandFactory {
+import Frame.AbsFactory;
+import Frame.Body;
+import Frame.Wheel;
 
+public class BbrandFactory extends AbsFactory {
+        private Body body;
+        private Wheel wheel;
+        
+        public Body createBody() {
+                // TODO Auto-generated method stub
+                return null;
+        }
+
+        public Wheel createWheel() {
+                // TODO Auto-generated method stub
+                return null;
+        }
 }
--- [Basic]AbsFactoryPattern/src/Frame/AbsFactory.java
+++ [Basic]AbsFactoryPattern/src/Frame/AbsFactory.java
@@ -1,11 +1,11 @@
 package Frame;
 
 public abstract class AbsFactory {
-        public abstract void createBody(Body body);
-        public abstract void createWheel(Wheel wheel);
+        public abstract Body createBody();
+        public abstract Wheel createWheel();
         
         public void assembleBicycle() {
-                createBody()
-                createWheel
+                createBody();
+                createWheel();
         }
 }
